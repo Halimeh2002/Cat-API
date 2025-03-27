f98ab15a0eafd249dd7ff691531ac8ad77a0318c
dear  Halimeh <nahalimeh@gmail.com>
2025-03-27 15:33:40 +0430

    "Cat Fact Api"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // fetch("https://catfact.ninja/fact")
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+
+  const [fact, setFact] = useState("");
+
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
+  const fetchFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      console.log(res);
+      setFact(res.data.fact);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchFact}>fetch data</button>
+      <p>{fact}</p>
     </div>
   );
 }
